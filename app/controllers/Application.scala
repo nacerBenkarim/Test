43975a98eb8b40dcdb151b403cd26652303f046f
mouradBkarim <mouradkarim@gmail.com>
2017-02-10 23:13:05 +0100

    commit for version v1.1.0

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Hello world : I am using tag in github now."))
+    Ok(views.html.index("Hello world : I am using tag in github now. this is the second test(version v1.1.0)"))
   }
 
 }
